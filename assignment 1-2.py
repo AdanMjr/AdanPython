8da2704c5803a6e82b2ed16d020e227d58719487
AdanMjr <adan032806@gmail.com>
2024-09-07 14:05:25 -0400

    Add files via upload

---
+++ assignment 1-2.py
@@ -0,0 +1,11 @@
+# Have the user to enter a value
+user_input = input("Enter a value: ")
+
+# Convert the input to a number
+value = int(user_input)
+
+# Multiply the value by 10
+result = value * 10
+
+# Output the result
+print("The result is:", result)
