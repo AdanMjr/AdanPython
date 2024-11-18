a9389f6ecf8ec5804869cad6a65d72f81e603bd8
AdanMjr <adan032806@gmail.com>
2024-11-18 18:55:16 -0500

    Add files via upload

---
+++ 0-2 Project AM.py
@@ -0,0 +1,36 @@
+AllowedVehiclesList = [
+    'Ford F-150', 
+    'Chevrolet Silverado', 
+    'Tesla CyberTruck', 
+    'Toyota Tundra', 
+    'Nissan Titan'
+]
+# The menu is basically the same as 0-1 just the change in 2 and addition of 3 and the input of the full name of the vehicle
+def Onload():
+    print("********************************")
+    print("AutoCountry Vehicle Finder v0.2")
+    print("********************************")
+    print("Please Enter the following number below from the following menu:")
+    
+    print("1. PRINT all Authorized Vehicles")
+    print("2. SEARCH for Authorized Vehicle")
+    print("3. Exit")
+    print("********************************")
+ # input 1 is the same as the 0-1 project assignment
+    user_input = input("Enter Your Choice: ")
+    if user_input == "1":
+        print("The AutoCountry sales manager has authorized the purchase of the following Vehicles: ", AllowedVehiclesList)
+        Onload()
+    # for input 2 I used AI to help me but it did help me understand what I was programming and I changed it to elif to allow multiple conditions
+    elif user_input == "2":
+        vehicle_to_search = input("Please Enter the full Vehicle name: ").strip()
+        if vehicle_to_search in AllowedVehiclesList:
+            print(vehicle_to_search, "is an authorized vehicle")
+        else:
+            print(vehicle_to_search, "is not an authorized vehicle, if you received this in error please check the spelling and try again")
+        Onload()
+        
+# input 3 is the same as the 0-1 project assignment but also changed to elif 
+    elif user_input == "3":
+        print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+Onload()
